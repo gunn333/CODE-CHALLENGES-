0c79a571c82060c6557de01fa5284293e1c62918
Gunn Arora <122155330+gunn333@users.noreply.github.com>
2024-10-08 10:34:39 +0530

    Leetcode 73

---
+++ JavaChallenge/Day4/Set_Matrix_Zeroes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+       boolean firstRow = false, firstCol = false;
+
+    // Set markers in first row and first column
+    for (int i = 0; i < matrix.length; i++)
+      for (int j = 0; j < matrix[0].length; j++)
+        if (matrix[i][j] == 0) {
+          if (i == 0) firstRow = true;
+          if (j == 0) firstCol = true;
+          matrix[0][j] = 0;
+          matrix[i][0] = 0;
+        }
+
+    // Replace inner matrix
+    for (int i = 1; i < matrix.length; i++)
+      for (int j = 1; j < matrix[0].length; j++)
+        if (matrix[i][0] == 0 || matrix[0][j] == 0)
+          matrix[i][j] = 0;
+
+    // Last remaining checks
+    if (firstRow)
+      for (int j = 0; j < matrix[0].length; j++)
+        matrix[0][j] = 0;
+
+    if (firstCol)
+      for (int i = 0; i < matrix.length; i++)
+        matrix[i][0] = 0; 
+    }
+}
